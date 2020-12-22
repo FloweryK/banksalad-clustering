05bacb0ffc15ad29c77d56bcb5a3d5537db7fd16
Minsang <flowerk94@gmail.com>
2020-12-22 15:50:42 +0900

    minor code mixup

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 plt.rcParams['font.family'] = 'AppleSDGothicNeoM00'
 
 import numpy as np
@@ -10,121 +11,198 @@ from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
 
 
-def run():
-    # load raw data
-    raw = pd.read_csv('src/2019-12-18~2020-12-18.csv', index_col='날짜')
-    raw.index = pd.to_datetime(raw.index)
-    raw = raw[raw['타입'] == '지출']
+class Summary:
+    def __init__(self, path, norm=False, mean=False, figsize=(10, 15)):
+        self.df = self.load_data(path, norm, mean)
+        self.fig = plt.figure(figsize=figsize)
 
-    # prerequisites
-    categories = sorted(list(set(raw['대분류'])))
-    dates = []
-    amounts = {
-        category: []
-        for category in categories
-    }
+    def load_data(self, path, norm, mean):
+        # load raw data
+        raw = pd.read_csv(path, index_col='날짜')
+        raw.index = pd.to_datetime(raw.index)
+        raw = raw[raw['타입'] == '지출']
 
-    # fill data
-    for month, group in raw.groupby(pd.Grouper(freq='W')):
-        dates.append(month)
+        # prerequisites
+        categories = sorted(list(set(raw['대분류'])))
 
-        for category in categories:
-            if category == 'total':
-                amounts[category].append(-group['금액'].sum())
+        dates = []
+        amounts = {category: [] for category in categories}
+
+        # fill data
+        for month, group in raw.groupby(pd.Grouper(freq='W')):
+            dates.append(month)
+
+            for category in categories:
+                if category == 'total':
+                    amounts[category].append(-group['금액'].sum())
+                else:
+                    amounts[category].append(-group[group['대분류'] == category]['금액'].sum())
+
+        # convert to dataframe
+        df = pd.DataFrame(amounts, index=dates)
+        df.index = df.index.strftime('%Y-%m-%d')
+        df = df[df.sum(axis=0).sort_values(ascending=False).index]
+
+        # normalize
+        if norm:
+            df = df.div(df.sum(axis=1), axis=0)
+        if mean:
+            df -= df.mean(axis=0)
+
+        return df
+
+    @staticmethod
+    def get_positions(df, measure):
+        if measure == 'cosine':
+            return 1 - cosine_similarity(df, df)
+        elif measure == 'euclidean':
+            return df
+        else:
+            raise KeyError('invalid measure options')
+
+    @staticmethod
+    def get_distance(df, measure):
+        if measure == 'cosine':
+            return 1 - cosine_similarity(df, df)
+        elif measure == 'euclidean':
+            return euclidean_distances(df)
+        else:
+            raise KeyError('invalid measure options')
+
+    @staticmethod
+    def plt_show():
+        plt.show()
+
+    @staticmethod
+    def plt_savefig(path):
+        plt.savefig(path)
+
+    @staticmethod
+    def plt_close():
+        plt.close()
+
+    def add_knee_finding(self, nrow, row, measure, tol):
+        # get positions
+        positions = self.get_positions(self.df, measure)
+
+        # make knee data
+        K = range(1, len(self.df.index))
+        seq = [KMeans(n_clusters=k, tol=tol).fit(positions).inertia_ for k in K]
+        dseq = [0] + [abs((seq[j + 1] - seq[j])) / (1e-5 + seq[j]) for j in range(len(seq) - 1)]
+        knee = 2
+        while knee < len(dseq):
+            # hard-coded options for dseq value
+            if (dseq[knee-1] > dseq[knee] < dseq[knee+1]) and (dseq[knee] < 0.12):
+                break
             else:
-                amounts[category].append(-group[group['대분류'] == category]['금액'].sum())
+                knee += 1
+        knee += 1
 
-    # convert to dataframe
-    df = pd.DataFrame(amounts, index=dates)
-    df = df.div(df.sum(axis=1), axis=0)
-    df.index = df.index.strftime('%Y-%m-%d')
+        # add subplot
+        ax = self.fig.add_subplot(nrow, 2, (1 + 2 * row, 2 + 2 * row))
+        ax.plot(K, seq, 'bx-')
+        ax.set_xlabel('K')
+        ax.set_ylabel('Sum_of_squared_distances')
+        ax.set_xticks(range(1, len(self.df.index), 2))
+        ax.set_title('Elbow Method For Optimal k')
+        ax.grid(which='both')
+        ax2 = ax.twinx()
+        ax2.plot(K, dseq, 'rx-')
+        ax2.text(knee-1, dseq[knee-1] - 0.04, 'knee: ' + str(knee), size=13)
+        ax2.set_ylabel('dJ')
 
-    # sort category order by sum
-    # categories = [category for _, category in sorted(zip(df.sum(axis=0), categories), reverse=True)]
+        return knee
+
+    def add_clustering_result(self, nrow, row, measure, n_clusters, tol):
+        # get positions
+        positions = self.get_positions(self.df, measure)
+
+        # make clustering data
+        cluster_index = {}
+        for label, index in zip(KMeans(n_clusters=n_clusters, tol=tol).fit(positions).labels_, self.df.index):
+            if label not in cluster_index:
+                cluster_index[label] = []
+            cluster_index[label].append(index)
+        cluster_index = {label: indices for label, indices in enumerate(sorted(list(cluster_index.values()), key=lambda l: len(l), reverse=True))}
+
+        # add subplot
+        # heatmap before & after clustering
+        ax1 = self.fig.add_subplot(nrow, 2, 1+2*row)
+        ax2 = self.fig.add_subplot(nrow, 2, 2+2*row)
+
+        # heatmap before clustering
+        im1 = self.get_distance(self.df, measure)
+        plt1 = ax1.imshow(im1, cmap='viridis_r')
+        cax1 = make_axes_locatable(ax1).append_axes("right", size="5%", pad=0.05)
+        cax1.axis('off')
+        ax1.set_xticks(np.arange(len(self.df.index)))
+        ax1.set_yticks(np.arange(len(self.df.index)))
+        ax1.set_xticklabels([])
+        ax1.set_yticklabels([])
+        ax1.set_title(measure + ' distance (before clustering)')
+
+        # heatmap after clustering
+        im2 = self.get_distance(pd.DataFrame(self.df, index=[index for label in cluster_index for index in cluster_index[label]]), measure)
+        plt2 = ax2.imshow(im2, cmap='viridis_r')
+        cax2 = make_axes_locatable(ax2).append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(plt2, cax=cax2)
+        ax2.set_xticks(np.arange(len(self.df.index)))
+        ax2.set_yticks(np.arange(len(self.df.index)))
+        ax2.set_xticklabels([])
+        ax2.set_yticklabels([])
+        ax2.set_title(measure + ' distance (after clustering)')
+
+        # put labels on heatmap
+        for label, indices in cluster_index.items():
+            x = sum([len(_indices) for _indices in list(cluster_index.values())[:label]]) - 0.5
+            ax2.text(x, x, '#' + str(label), verticalalignment='top', horizontalalignment='left')
+            ax2.axvline(x, color='white')
+            ax2.axhline(x, color='white')
+
+        # grouped bar chart
+        for i, (label, indices) in enumerate(cluster_index.items()):
+            grouped_df = self.df.loc[indices]
+            grouped_df = grouped_df.div(grouped_df.sum(axis=1), axis=0)
+
+            ax3 = self.fig.add_subplot(nrow, n_clusters, 1+n_clusters*(row+1)+label)
+
+            bottom = np.zeros(len(grouped_df.index))
+            for category in grouped_df:
+                ax3.bar(grouped_df.index, grouped_df[category].to_numpy(), bottom=bottom, label=category)
+                bottom += grouped_df[category].to_numpy()
+
+            if i == len(cluster_index)-1:
+                self.fig.subplots_adjust(right=0.7)
+                ax3.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+            ax3.set_title('#' + str(label))
+            ax3.set_xticklabels(grouped_df.index, fontsize=6, rotation=90)
+            ax3.set_yticklabels([])
+            ax3.set_ylim([0, 1])
+
+
+def run():
+    path = 'src/2019-12-18~2020-12-18.csv'
+    norm = True
+    mean=True
+    tol = 1e-7
+    measure = 'cosine'
 
     '''
-    # stacked bar plot
-    fig, axes = plt.subplots(figsize=(15, 5))
-    bottom = np.zeros(len(df.index))
-    for category in categories:
-        axes.bar(df.index, df[category].to_numpy(), bottom=bottom, label=category)
-        bottom += df[category].to_numpy()
-
-    axes.set_ylabel('지출 구성')
-    axes.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
-    # plt.grid(True, axis='y')
-    plt.xticks(df.index, rotation=90)
-    plt.title('월 지출')
-    plt.tight_layout()
-    plt.show()
+    summary = Summary(path=path, norm=norm, mean=mean, figsize=(10, 15))
+    knee = summary.add_knee_finding(nrow=3, row=0, measure=measure, tol=tol)
+    summary.add_clustering_result(nrow=3, row=1, measure=measure, n_clusters=knee, tol=tol)
+    summary.plt_show()
     '''
 
-    '''
-    # simple line plot
-    fig, axes = plt.subplots(figsize=(20, 5))
-    for i, category in enumerate(categories):
-        axes.plot(df3[category], label=category, linestyle='-', linewidth=1., marker='o', markersize=4)
-
-    # axis settings
-    axes.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.tight_layout()
-    plt.show()
-    '''
-
-    # cluster number determination
-    '''
-    fig, ax1 = plt.subplots()
-    ax2 = ax1.twinx()
-
-    Sequence = []
-    K = range(1, len(df.index))
-    for k in K:
-        km = KMeans(n_clusters=k).fit(df)
-        Sequence.append(km.inertia_)
-
-    dSequence = [0] + [abs((Sequence[j+1]-Sequence[j]))/(1e-5 + Sequence[j]) for j in range(len(Sequence)-1)]
-    ax1.plot(K, Sequence, 'bx-')
-    ax2.plot(K, dSequence, 'rx-')
-    ax1.set_xlabel('K')
-    ax1.set_ylabel('Sum_of_squared_distances')
-    ax2.set_ylabel('dJ')
-    ax1.set_xticks(range(1, len(df.index), 2))
-    ax1.grid(which='both')
-    plt.title('Elbow Method For Optimal k')
-    plt.tight_layout()
-    plt.show()
-    '''
-
-    # clustering
-    clustered = KMeans(n_clusters=7).fit(df)
-    labels = clustered.labels_
-
-    # sort index order by clustering
-    clustered_index = [index for _, index in sorted(zip(labels, df.index))]
-
-    # visualization - heatmap
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
-
-    # subplot 0: heatmap before clustering
-    axes[0].imshow(euclidean_distances(df, df), cmap='viridis_r')
-    axes[0].set_xticks(np.arange(len(df.index)))
-    axes[0].set_yticks(np.arange(len(df.index)))
-    axes[0].set_xticklabels(df.index, rotation=90, fontsize=7)
-    axes[0].set_yticklabels(df.index, fontsize=7)
-    axes[0].set_title('cosine sim. heatmap (before clustering)')
-
-    # subplot 1: heatmap after clustering
-    df = pd.DataFrame(df, index=clustered_index)
-    axes[1].imshow(euclidean_distances(df, df), cmap='viridis_r')
-    axes[1].set_xticks(np.arange(len(df.index)))
-    axes[1].set_yticks(np.arange(len(df.index)))
-    axes[1].set_xticklabels(df.index, rotation=90, fontsize=7)
-    axes[1].set_yticklabels(df.index, fontsize=7)
-    axes[1].set_title('cosine sim. heatmap (after clustering)')
-
-    fig.tight_layout()
-    plt.show()
+    for norm in [True, False]:
+        for mean in [True, False]:
+            for measure in ['cosine', 'euclidean']:
+                print(norm, mean, measure)
+                summary = Summary(path=path, norm=norm, mean=mean, figsize=(10, 15))
+                knee = summary.add_knee_finding(nrow=3, row=0, measure=measure, tol=tol)
+                summary.add_clustering_result(nrow=3, row=1, measure=measure, n_clusters=knee, tol=tol)
+                summary.plt_savefig('measure=%s_norm=%s_mean=%s.jpg' % (str(measure), str(norm), str(mean)))
+                summary.plt_close()
 
 
 if __name__ == '__main__':
